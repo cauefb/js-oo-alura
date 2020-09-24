d77277898d3b1e5a9a57f8979d4c0a0cb0afdc70
cauê ferreira baraúna <kau3hc@hotmail.com>
2020-09-24 18:56:08 -0300

    Aula 2

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 class Cliente{
     nome;
     cpf;
+   
+}
+
+class ContaCorrente{
     agencia;
-    saldo;
-    rg;
+    _saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+    depositar(valor) {
+        if(valor <= 0) {
+            return;
+        }
+        this._saldo += valor;
+    }
 }
 
 const cliente1 = new Cliente;
@@ -11,12 +27,21 @@ const cliente2 = new Cliente ;
 
 cliente1.nome="Ricardo";
 cliente1.cpf =12312312312;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+
 
 cliente2.nome="Alice";
 cliente2.cpf =123123344312312;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contacorrenteRicardo = new ContaCorrente();
+
+contacorrenteRicardo.saldo =0;
+contacorrenteRicardo.agencia = 1001;
+console.log(contacorrenteRicardo.saldo);
+contacorrenteRicardo.saldo = 100;
+console.log(contacorrenteRicardo.saldo);
+contacorrenteRicardo.sacar(50);
+
+console.log(contacorrenteRicardo.saldo);
+
 
 console.log(cliente1, cliente2)
